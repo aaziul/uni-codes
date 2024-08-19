4280b71e7b1355a0ab32a83a0d6f08e79ed5d404
luiza <97320223+aaziul@users.noreply.github.com>
2024-08-19 08:56:41 -0300

    adicionando o data.h da atividade sobre bfs e dfs

---
+++ estrutura-de-dados/bfs-dfs/data.h
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct sTipoFila{
+    int info;
+    struct sTipoFila* prox;
+};
+
+typedef struct sTipoFila TipoFila;
+
+TipoFila* criaFila(void);
+TipoFila* insereFila (TipoFila *l, int dado);
+int filaVazia (TipoFila* l);
+int removeFila(TipoFila **l);
+void imprimeFila(TipoFila* l);
+void DFS(int matrizAdj[8][8], int vertice, int visitado[]);
+void BFS(int grafo[8][8], int vertice);
